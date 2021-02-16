d6731a3982c7b5c1e0df5ea001353c3ee91cb301
Diego Pajarito <diegopajarito@gmail.com>
2021-02-16 15:13:49 -0500

    Resource Scrapy library for session 3

---
+++ session3/tutorial/tutorial/spiders/quotes_spider_a.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes_a"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+            'https://iaac.net/educational-programmes/masters-programmes/master-in-city-technology/#faculty'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
